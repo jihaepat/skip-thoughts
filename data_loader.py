96a0941570c6b3bb714398bd6e8c0994aa05bae7
gulby <gulby@jihaepat.com>
2018-09-28 12:55:20 +0900

    dictionary 사용 제거 : spm 이 생성한 id 값을 그대로 사용

--- data_loader.py
+++ data_loader.py
@@ -19,64 +19,31 @@ class DataLoader:
             print("Loading text file at {}".format(text_file))
             with open(text_file, "rt") as f:
                 sentences = f.readlines()
-            print("Making dictionary for these words")
-            word_dict = build_and_save_dictionary(sentences, source=text_file)
 
-        assert sentences and word_dict, "Please provide the file to extract from or give sentences and word_dict"
+        assert sentences
 
         self.sentences = sentences
-        self.word_dict = word_dict
         print("Making reverse dictionary")
-        self.revmap = list(self.word_dict.items())
 
         self.lengths = [len(sent) for sent in self.sentences]
 
     def convert_sentence_to_indices(self, sentence):
-
-        indices = [
-                      # assign an integer to each word, if the word is too rare assign unknown token
-                      self.word_dict.get(w) if self.word_dict.get(w, VOCAB_SIZE + 1) < VOCAB_SIZE else UNK
-
-                      for w in sentence.split()  # split into words on spaces
-                  ][: MAXLEN - 1]  # take only maxlen-1 words per sentence at the most.
-
-        # last words are EOS
-        indices += [EOS] * (MAXLEN - len(indices))
-
-        indices = np.array(indices)
+        # np.array
+        indices = np.empty(MAXLEN, dtype=np.int)
+        indices.fill(EOS)
+        indices[0] = BOS
+        for i, w in enumerate(sentence.split()[:MAXLEN-2]):
+            indices[i+1] = int(w)
+        # torch.Variable
         indices = Variable(torch.from_numpy(indices))
         if USE_CUDA:
             indices = indices.cuda(CUDA_DEVICE)
-
         return indices
 
     def convert_indices_to_sentences(self, indices):
-
-        def convert_index_to_word(idx):
-
-            idx = idx.data[0]
-            if idx == EOS:
-                return '2'
-            elif idx == BOS:
-                return '1'
-            elif idx == UNK:
-                return '0'
-
-            search_idx = idx - 3
-            if search_idx >= len(self.revmap):
-                return "NA"
-            
-            word, idx_ = self.revmap[search_idx]
-
-            assert idx_ == idx
-            return word
-
-        words = [convert_index_to_word(idx) for idx in indices]
-
-        return " ".join(words)
+        return ' '.join([str(int(idx)) for idx in indices])
 
     def fetch_batch(self, batch_size):
-
         first_index = random.randint(0, len(self.sentences) - batch_size)
         batch = []
         lengths = []
