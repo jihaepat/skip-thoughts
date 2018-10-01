8389ea0392b3eba6275fa432fe788ee7e272f953
gulby <gulby@jihaepat.com>
2018-10-01 16:35:00 +0900

    encoding 후 normalize 하여 저장

--- encoder.py
+++ encoder.py
@@ -54,20 +54,24 @@ def process_batch():
         for j, w in enumerate(splits[:MAXLEN - 1]):
             sentences[i][j] = int(w)
     # numpy --> torch
-    batch = Variable(torch.from_numpy(sentences))
+    batch = Variable(torch.from_numpy(sentences))   ;assert batch.shape == (batch_size, MAXLEN)
     if USE_CUDA:
         batch = batch.cuda(CUDA_DEVICE)
-    encodings, _ = encoder(batch)
-    encodings = encodings.view(-1, THOUGHT_SIZE)
-    encodings = encodings.data.cpu().numpy()
+    encodings, _ = encoder(batch)                   ;assert encodings.shape == (batch_size, THOUGHT_SIZE)
+    norms = encodings.norm(dim=1)                   ;assert norms.shape == (batch_size,)
+    norms_for_normalize = norms.clamp(1e-12).expand(THOUGHT_SIZE, batch_size).transpose(0, 1)
+    normalized_encodings = encodings / norms_for_normalize      ;assert normalized_encodings.shape == (batch_size, THOUGHT_SIZE)
+    normalized_encodings = normalized_encodings.data.cpu().numpy()
+    norms = norms.data.cpu().numpy()
     # save
-    for line, encoding in zip(lines, encodings):
+    for line, normalized, norm in zip(lines, normalized_encodings, norms):
+        # assert 0.99 <= np.sqrt(np.dot(normalized, normalized)) <= 1.01
+        assert normalized.shape == (THOUGHT_SIZE,)
         h = CityHash128(line)
         ht = '000000{}'.format(h)
         path = './encodings/{}/{}'.format(ht[-3:], ht[-6:-3])
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
-        assert encoding.shape == (THOUGHT_SIZE,)
-        np.save('{}/{}.npy'.format(path, h), encoding)
+        np.save('{}/{}_{}.npy'.format(path, h, norm), normalized)
     # finalize
     sentences.fill(EOS)
     lines = []
